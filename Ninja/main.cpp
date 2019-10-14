d20507560c7d705f6127bd6d5a1bbd09131872ab
marcelismael1 <55630904+marcelismael1@users.noreply.github.com>
2019-10-15 00:09:44 +0200

    Added input player name

--- Ninja/main.cpp
+++ Ninja/main.cpp
@@ -5,8 +5,21 @@ using namespace std;
 
 int main()
 {
+    string Your_First_Name;
+    string Your_Last_Name;
     cout << "Start \n";
-    cout << Ninja_Naming("marcel","ismael") << endl;
+
+    cout<<"Enter your first name:";
+    cin>>Your_First_Name;
+
+
+    cout<<endl<<"Enter your last name:";
+    cin>>Your_Last_Name;
+
+
+    cout <<endl<<"Ninja Name is: "<< Ninja_Naming(Your_First_Name,Your_Last_Name) << endl;
+
+
     cout << "End \n";
     return 0;
 }
